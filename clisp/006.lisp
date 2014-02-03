5cdac194f7a6cf286b24d4a2133a539fec87e94b
辻 隆太郎 <kametaro-main@tsuji-no-Mac-mini.local>
2014-02-04 07:19:08 +0900

    square func add

--- clisp/006.lisp
+++ clisp/006.lisp
@@ -1,10 +1,12 @@
+(defun square (n) (* n n))
+
 (defun my-sum (num)
   (loop for i from 1 to num sum (+ i)))
 
 (defun my-square-sum (num)
-  (loop for i from 1 to num sum (* i i)))
+  (loop for i from 1 to num sum (square i)))
 
-(- (* (my-sum 100) (my-sum 100)) (my-square-sum 100))
+(- (square (my-sum 100)) (my-square-sum 100))
 
 
 
